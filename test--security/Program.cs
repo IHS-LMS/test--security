b562ea6e0945687e5d956d6b7cad44ddd0ff2b95
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2020-11-19 17:00:59 +0430

    Add project files.

---
+++ test--security/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Permissions;
+using System.Security.Principal;
+
+namespace test__security
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
+            PrincipalPermission principalPerm = new PrincipalPermission(null, "Administrators");
+            principalPerm.Demand();
+            Console.WriteLine("Demand succeeded.");
+            Console.Read();
+        }
+    }
+}
